f7979e44b4f2b6ec77f67a85b3f766f17a9c24f7
Aksh Ravi <akshravishankar@cmail.carleton.ca>
2023-01-21 22:02:46 -0500

    Added client.py, implemented large parts of server.py

--- 4502/Assignment1/Server.py
+++ 4502/Assignment1/Server.py
@@ -16,7 +16,8 @@ class Server:
 
     # Conn vars
     port = None
-    def startUP(self, portNum) -> None:
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    def startUP(self, portNum=12000) -> None:
         '''
         Initiate class and populate data structures from text files (database)
         '''
@@ -37,6 +38,9 @@ class Server:
                 else:
                     self.resDict[line[0]] += [line[1]]
 
+        '''
+        Read info from car list and date list files and store in local data structures
+        '''
         with open(self.carListFilepath, 'r') as carFile:
             for car in carFile:
                 self.carList.append(car.strip())
@@ -45,19 +49,98 @@ class Server:
             for date in dateFile:
                 self.dateList.append(date.strip())
 
+        # Assign IP address and port number to socket 
+        # using loopback address
+        self.serverSocket.bind(('127.0.0.1', self.port)) 
+
         # Once initial setup is done, wait for requests
         self.waitForRequests()
 
     def waitForRequests(self) -> None:
         # Remain idle until requests arrive, handle requests as needed
-        print(self.carList)
-        print(self.dateList)
-        print(self.resDict)
-        
-        
-        # Call necessary functions to handle request
-        # while True:
-        # wait for and respond to requests
+        # print(self.carList)
+        # print(self.dateList)
+        # print(self.resDict)
+
+        socket = self.serverSocket
+        while True:    
+            message, address = socket.recvfrom(1024)
+
+            match message.split()[0]:
+                case 'cars':
+                    returnMessage = self.composeCarMessage()
+                    socket.sendto(returnMessage, address)
+                
+                case 'dates':
+                    returnMessage = self.composeDateMessage()
+                    socket.sendto(returnMessage, address)
+                
+                case 'check':
+                    returnMessage = self.composeResMessage()
+                    socket.sendto(returnMessage, address)
+                
+                case 'reserve':
+                    car = message.split[1]
+                    date = message.split[2]
+                    if car not in self.resDict.keys() and car not in self.carList:
+                        returnMessage = 'Invalid car'
+                        socket.sendto(bytes(returnMessage), address)
+                    if date not in self.dateList:
+                        returnMessage = 'Invalid reservation date'
+                        socket.sendto(bytes(returnMessage), address)
+                    
+                    self.addRes((car, date))
+                    
+                    socket.sendto(bytes('Reservation Confirmed'), address)
+                case 'delete':
+                    
+                    # TODO: Implement delete code and function 
+                    
+                    socket.sendto(bytes('Reservation Deleted'), address)
+
+                case 'quit':
+
+                    # TODO: Implement quit code and function
+
+                    socket.sendto(bytes('Server shutting down'), address)
+                    return
+
+
+    def composeCarMessage(self):
+        cList = self.carList
+        message = []
+        for c in cList:
+            message.append(bytes(c, 'utf-8'))
+        return message
+
+    def composeDateMessage(self):
+        dList = self.dateList
+        message = []
+        for d in dList:
+            message.append(bytes(d, 'utf-8'))
+        return message
+    
+    def composeResMessage(self):
+        resMessage = []
+        rList = self.resDict
+        resCars = rList.keys()
+
+        for car in resCars:
+            resMessage.append(bytes(car, 'utf-8'))
+            for res in rList[car]:
+                resMessage.append(bytes(res, 'utf-8'))
+
+        return resMessage
+
+    def addRes(self, resMessage):
+        car = resMessage[0]
+        date = resMessage[1]
+
+        if car not in self.resDict.keys():
+            self.resDict[car] = [date]
+        else:
+            self.resDict[car] += [date]
+
         return
 
 if __name__ == "__main__":
