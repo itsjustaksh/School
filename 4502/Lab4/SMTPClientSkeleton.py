edd18cf7b51c9c0f5f4a5416ea08beb403ec0fac
itsjustme7864 <aksh.ravishankar@gmail.com>
2023-02-28 17:40:38 -0500

    Established connection with server, send mailing addr
    SYSC 4502

--- 4502/Lab4/SMTPClientSkeleton.py
+++ 4502/Lab4/SMTPClientSkeleton.py
@@ -6,10 +6,15 @@ endmsg = '\r\n.\r\n'
 
 # Choose a mail server and call it mailserver
 mailserver = '127.0.0.1'
+mailserverPort = 5555
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
 
 #Fill in start   
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.settimeout(5)
+clientSocket.connect((mailserver, mailserverPort))
 
 #Fill in end 
 
@@ -30,12 +35,31 @@ if recv1[:3] != '250':
 
 #Fill in start   
 
+clientSocket.send("MAIL FROM:<noreply@mailserver.tld>\r\n".encode())
+try:
+	resp = clientSocket.recv(1024).decode()
+	if '250' not in resp:
+		print("Bad request, 250 reply not recieved")
+	else:
+		print(resp)
+except TimeoutError as t:
+	print("Mail From Timeout, no response")
+
 #Fill in end 
 
 # Send RCPT TO command and print server response. 
 
 #Fill in start   
 
+clientSocket.send("RCPT TO\r\n")
+try:
+	resp = clientSocket.recv(1024).decode()
+	if '250' not in resp:
+		print("Bad request, 250 reply not recieved")
+	else:
+		print(resp)
+except TimeoutError as t:
+	print("RCPT TO Timeout, no response")
 #Fill in end 
 
 # Send DATA command and print server response. 
