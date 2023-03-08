cc039e84689e9272f7f1c7a104a0e9fe17098d97
itsjustme7864 <aksh.ravishankar@gmail.com>
2023-03-08 11:21:26 -0500

    A2 Setup
    SYSC 4504

---
+++ 4504/Assignment 2/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>SYSCBOOK - Main</title>
+    <link rel="stylesheet" href="assets/css/reset.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>SYSCBOOK</h1>
+      <p>Social media for SYSC students in Carleton University</p>
+    </header>
+    <nav>
+      <ul class="navbar">
+        <li>
+          <a href="index.html"><strong>Home</strong></a>
+        </li>
+        <li>
+          <a href="profile.html"><strong>Profile</strong></a>
+        </li>
+        <li>
+          <a href="register.html"><strong>Register</strong></a>
+        </li>
+        <li>
+          <a href="index.html"><strong>Logout</strong></a>
+        </li>
+      </ul>
+    </nav>
+
+    <main>
+      <section>
+        <form method="POST" action="https://ramisabouni.com/sysc4504/process_index.php">
+          <fieldset>
+            <table>
+              <tr>
+                <td>
+                  <legend><p>New Post</p></legend>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <textarea
+                    name="new_post"
+                    id="new_post_input"
+                    rows="10"
+                    cols="45"
+                    placeholder="What's on your mind? (Max 500 char)"
+                  ></textarea>
+                </td>
+              </tr>
+              <tr>
+                <td class="buttons">
+                  <input type="submit" value="Post" />
+                  <input type="reset" value="Reset" />
+                </td>
+                <!-- <td>&nbsp;</td> -->
+              </tr>
+            </table>
+          </fieldset>
+        </form>
+
+        <details open class="post">
+          <summary>Post 1</summary>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque faucibus accumsan. Proin in aliquet nulla. Integer
+            faucibus nibh lobortis sapien eleifend vulputate. Mauris accumsan
+            augue eu erat posuere ornare. Ut vestibulum aliquam lacinia.
+            Pellentesque ac augue nisl. Cras laoreet faucibus efficitur. Aenean
+            vitae sapien convallis, auctor magna vitae, suscipit ipsum. Nam
+            maximus euismod feugiat. Suspendisse a metus metus. Donec rhoncus
+            aliquam nisl, quis interdum quam dignissim euismod. Proin sed mattis
+            ante, id vestibulum arcu. Aliquam ac tristique mauris. Integer et
+            placerat nulla, cursus porttitor felis.
+          </p>
+        </details>
+
+        <details open class="post">
+          <summary>Post 2</summary>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque faucibus accumsan. Proin in aliquet nulla. Integer
+            faucibus nibh lobortis sapien eleifend vulputate. Mauris accumsan
+            augue eu erat posuere ornare. Ut vestibulum aliquam lacinia.
+            Pellentesque ac augue nisl. Cras laoreet faucibus efficitur. Aenean
+            vitae sapien convallis, auctor magna vitae, suscipit ipsum. Nam
+            maximus euismod feugiat. Suspendisse a metus metus. Donec rhoncus
+            aliquam nisl, quis interdum quam dignissim euismod. Proin sed mattis
+            ante, id vestibulum arcu. Aliquam ac tristique mauris. Integer et
+            placerat nulla, cursus porttitor felis.
+          </p>
+        </details>
+
+        <details open class="post">
+          <summary>Post 3</summary>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque faucibus accumsan. Proin in aliquet nulla. Integer
+            faucibus nibh lobortis sapien eleifend vulputate. Mauris accumsan
+            augue eu erat posuere ornare. Ut vestibulum aliquam lacinia.
+            Pellentesque ac augue nisl. Cras laoreet faucibus efficitur. Aenean
+            vitae sapien convallis, auctor magna vitae, suscipit ipsum. Nam
+            maximus euismod feugiat. Suspendisse a metus metus. Donec rhoncus
+            aliquam nisl, quis interdum quam dignissim euismod. Proin sed mattis
+            ante, id vestibulum arcu. Aliquam ac tristique mauris. Integer et
+            placerat nulla, cursus porttitor felis.
+          </p>
+        </details>
+      </section>
+    </main>
+  </body>
+</html>
---
+++ 4504/Assignment 2/profile.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Update SYSCBOOK profile</title>
+    <link rel="stylesheet" href="assets/css/reset.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>SYSCBOOK</h1>
+      <p>Social media for SYSC students in Carleton University</p>
+    </header>
+    <nav>
+      <ul class="navbar">
+        <li>
+          <a href="index.html"><strong>Home</strong></a>
+        </li>
+        <li>
+          <a href="profile.html"><strong>Profile</strong></a>
+        </li>
+        <li>
+          <a href="register.html"><strong>Register</strong></a>
+        </li>
+        <li>
+          <a href="index.html"><strong>Logout</strong></a>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <section>
+        <h2>Update Profile information</h2>
+        <form method="POST" action="https://ramisabouni.com/sysc4504/process_profile.php">
+          <fieldset>
+            <table>
+              <tbody>
+                <tr>
+                  <td colspan="3">
+                    <legend><p>Personal information</p></legend>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="profile-rows">
+                    <label><p>First Name:</p></label>
+                    <input name="first_name" type="text" />
+                  </td>
+                  <td class="profile-rows">
+                    <label><p>Last Name:</p></label>
+                    <input name="last_name" type="text" />
+                  </td>
+                  <td class="profile-rows">
+                    <label><p>Date of Birth:</p></label>
+                    <input name="DOB" type="date" name="DOB" id="profile_dob_input" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </fieldset>
+          <fieldset>
+            <table>
+              <tbody>
+                <tr>
+                  <td colspan="3">
+                    <legend><p>Address</p></legend>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="profile-rows">
+                    <label><p>Street Number:</p></label>
+                    <input name="street_number" type="number" />
+                  </td>
+                  <td class="profile-rows" colspan="2">
+                    <label><p>Street Name:</p></label>
+                    <input name="street_name" type="text" />
+                  </td>
+                </tr>
+                <tr>
+                  <td class="profile-rows">
+                    <label><p>City:</p></label>
+                    <input name="city" type="text" />
+                  </td>
+                  <td class="profile-rows">
+                    <label><p>Province:</p></label>
+                    <input name="provence" type="text" />
+                  </td>
+                  <td class="profile-rows">
+                    <label><p>Postal Code:</p></label>
+                    <input name="postal_code" type="text" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </fieldset>
+          <fieldset>
+            <table>
+              <tbody>
+                <tr>
+                  <td colspan="5">
+                    <legend><p>Profile information</p></legend>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="profile-rows">
+                    <label><p>Email address:</p></label>
+                    <input name="student_email" type="email" />
+                  </td>
+                </tr>
+                <tr>
+                  <td class="profile-rows">
+                    <label><p>Program:</p></label>
+                    <select name="Program">
+                      <option>Choose Program</option>
+                      <option>Computer Systems Engineering</option>
+                      <option>Software Engineering</option>
+                      <option>Communications Engineering</option>
+                      <option>Biomedical and Electrical Engineering</option>
+                      <option>Electrical Engineering</option>
+                      <option>Special</option>
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <p>Choose Avatar:</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <p>
+                      <input name="avatar" type="radio" value="avatar-1" />
+                      <label>
+                        <img src="images/img_avatar1.png" alt="avatar-icon" />
+                      </label>
+                      <input name="avatar" type="radio" value="avatar-2" />
+                      <label>
+                        <img src="images/img_avatar2.png" alt="avatar-icon" />
+                      </label>
+                      <input name="avatar" type="radio" value="avatar-3" />
+                      <label>
+                        <img src="images/img_avatar3.png" alt="avatar-icon" />
+                      </label>
+                      <input name="avatar" type="radio" value="avatar-4" />
+                      <label>
+                        <img src="images/img_avatar4.png" alt="avatar-icon" />
+                      </label>
+                      <input name="avatar" type="radio" value="avatar-5" />
+                      <label>
+                        <img src="images/img_avatar5.png" alt="avatar-icon" />
+                      </label>
+                    </p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <input name="submit" type="submit" value="Submit" />
+                    <input name="reset" type="reset" value="Reset" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </fieldset>
+        </form>
+      </section>
+    </main>
+  </body>
+</html>
