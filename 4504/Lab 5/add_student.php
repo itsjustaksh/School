f17b9ac3b46e364682faea53faca04628a4d84b8
itsjustme7864 <aksh.ravishankar@gmail.com>
2023-03-10 11:23:14 -0500

    Lab 5 progress - Part 2 complete, part 3 IP
    SYSC 4504

--- 4504/Lab 5/add_student.php
+++ 4504/Lab 5/add_student.php
@@ -28,3 +28,42 @@
 </body>
 
 </html>
+
+<?php
+
+    include("connection.php");
+    $conn = new mysqli($serverName, $username, $pass, $dbName);
+
+    if ($conn->connect_error) {
+        die("Error: Couldn't connect to database.<br>" . $conn->connect_error);
+    }
+
+    echo("Connected successfully<br>");
+
+    $name = 0;
+    $id = 0;
+    $DOB = 0;
+    $income = 0;
+    $courseID = 0;
+
+    if (isset($_POST["Name"])) {
+        $name = $_POST["Name"];
+        $id = $_POST["ID"];
+        $DOB = $_POST["DOB"];
+        $income = $_POST["Income"];
+        $courseID = $_POST["CourseID"];
+
+        $sqlLine = "INSERT INTO student (ID, NAME, DOB, INCOME, COURSE_ID) VALUES (
+            '$id', '$name', '$DOB', '$income', '$courseID')";
+    
+        //TODO: Add content to db
+        try {
+            $conn -> query($sqlLine);
+        } catch (Exception $th) {
+            echo("Could not add entry, sql error");
+        }
+    }
+
+    $conn->close();
+
+?>
