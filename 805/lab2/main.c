86937f5d2c25bd2325d81668e34855b53e6bcb87
Aksh Ravi <aksh.ravishankar@gmail.com>
2024-01-16 14:30:06 -0500

    Lab 2 start and outline

---
+++ 805/lab2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   // Step 1: Vars and givens
+   register int tempReg = 0x417164;
+   __uint8_t frzTemp = 0x7F; // Greater than this means freezing, else not
+
+   // Step 2: Init Reg with Positive Val
+
+   // Step 3: Polling
+   return 0;
+}
