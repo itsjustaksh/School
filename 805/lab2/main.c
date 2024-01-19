7313338da95f4793b3df5f1e912bd13295e53f84
Aksh Ravi <aksh.ravishankar@gmail.com>
2024-01-19 13:29:40 -0500

    805 Lab 2 Complete

--- 805/lab2/main.c
+++ 805/lab2/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
 
 int main()
 {
    // Step 1: Vars and givens
-   register int tempReg = 0x417164;
-   __uint8_t frzTemp = 0x7F; // Greater than this means freezing, else not
+   register uint8_t temperature;
+   uint8_t frzTemp = 0x7F; // Greater than this means freezing, else not
 
    // Step 2: Init Reg with Positive Val
+   temperature = 0x8F;
 
    // Step 3: Polling
+   while(1)
+   {
+      if (temperature > frzTemp)
+      {
+         printf("Freezing, ");
+      }
+      printf("Current temp: %u\n", temperature);
+      sleep(1);
+   }
    return 0;
 }
